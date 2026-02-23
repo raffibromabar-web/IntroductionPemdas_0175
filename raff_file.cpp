016b1493095abd789b169a52c425877e3ff23032
raffibromabar-web <raffibromabar@gmail.com>
2026-02-23 15:42:42 +0700

    first commit

---
+++ raff_file.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//variable lobal
+string nama;
+
+int main(){ //program utama
+    int age; //variable local
+    int age1; //variable local
+    age = 20;
+    age1 = 15;
+
+    cout << "Halo world" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+
+}
